7b35dfdbe4623800c3cb57669062740a015990ca
A2kmoise <abayomoise950@gmail.com>
2025-09-08 09:57:33 +0200

    -{ * * }- adv

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { UserModule } from './user/user.module';
+import { CartModule } from './cart/cart.module';
+import { OrderModule } from './order/order.module';
+import { PaymentModule } from './payment/payment.module';
+import { NotificationsModule } from './notifications/notifications.module';
 import configuration from './config/configuration';
 
 @Module({
@@ -14,6 +19,11 @@ import configuration from './config/configuration';
     }),
     PrismaModule,
     AuthModule,
+    UserModule,
+    CartModule,
+    OrderModule,
+    PaymentModule,
+    NotificationsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
